4568a693937a024d934b3d2fdf1239d5b174a4aa
lucamartinn <demaio.martin@gmail.com>
2019-07-23 23:36:12 -0300

    borrando filas de topic0

--- Topic_0/src/Abstract_Factory/IFabricaAbstracta.java
+++
@@ -1,9 +0,0 @@
-package Abstract_Factory;
-
-public interface IFabricaAbstracta {
-
-    IConexion getBD(String motor);
-
-    //here we could put all the factories we want.
-
-}
--- Topic_0/src/Proxy/ICuenta.java
+++
@@ -1,6 +0,0 @@
-package Proxy;
-
-public interface ICuenta {
-    Conexion conectarBD( Conexion conexion);
-    Conexion desconectarBD( Conexion conexion);
-}
